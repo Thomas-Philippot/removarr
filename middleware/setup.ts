3b44dd1e526173de761924b11240cc0ee795dacd
Thomas <github.theme991@slmails.com>
2025-05-23 16:27:11 +0200

    fix plex on fixed ip

--- middleware/setup.ts
+++ middleware/setup.ts
@@ -2,13 +2,12 @@ import type { MainSettings } from "~/server/repository/settingRepository";
 
 export default defineNuxtRouteMiddleware(async () => {
   const settings = await $fetch<MainSettings>("/api/settings");
-  const { data, token } = useAuth();
+  const { data } = useAuth();
   if (
     !settings.radarr.hostname ||
     !settings.radarr.apiKey ||
     !settings.sonarr.hostname ||
-    !settings.sonarr.apiKey ||
-    !settings.plex.api_uuid
+    !settings.sonarr.apiKey
   ) {
     if (data.value && data.value.role === "user") {
       // set user as admin
@@ -17,16 +16,6 @@ export default defineNuxtRouteMiddleware(async () => {
         body: {
           id: data.value.id,
           role: "admin",
-        },
-      });
-    }
-
-    if (token.value) {
-      // persist plex data to app settings
-      await $fetch("/api/plex/setup", {
-        method: "POST",
-        body: {
-          token: token.value,
         },
       });
     }
